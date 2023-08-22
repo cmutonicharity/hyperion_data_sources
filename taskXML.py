f56c3876caf707beadc2e6090f24fef468618641
Charity Mutoni <charity_91@outlook.com>
2023-08-22 20:48:15 +0100

    changed not_fav_count to unfavourable_count in taskXML.py

--- taskXML.py
+++ taskXML.py
@@ -6,14 +6,14 @@ for child in root.iter('movie'): # for is created to list all the child tag inea
     for description in root.iter('description'): 
         print(''.join(description.itertext()))
     favourite_count = 0 # variable created to count to the number of the fav
-not_fav_count = 0 # variable created to count to the number of the not fav
+unfavourable_count = 0 # variable created to count to the number of the not fav
 
 for movie in root.iter('movie'): # for loop is created to iterate all the movie's in root
     if 'favourite' in movie.attrib: # checking the favourite movie
         if movie.attrib['favourite'] == 'yes':
             favourite_count += 1 # counting to 1
         else:
-            not_fav_count += 1 # counting to 1
+            unfavourable_count += 1 # counting to 1
 
 print('Number of favorite movies: ', favourite_count)
-print('Number of non-favorite movies: ', not_fav_count)
+print('Number of non-favorite movies: ', unfavourable_count)
